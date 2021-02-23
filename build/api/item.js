b6e87581da6538db954d7013b788a2040a833868
Vlad Ageev <vageev06@gmail.com>
2021-02-23 11:50:39 +0200

    Added security checks

--- build/api/item.js
+++ build/api/item.js
@@ -7,18 +7,21 @@ function basicRoute(server) {
     server.get("/getItem", (req, reply) => {
         const total = req.query;
         const type = total.type;
-        mongodb_2.default(({ db, client }) => tslib_1.__awaiter(this, void 0, void 0, function* () {
-            let data;
-            if (type === "*")
-                data = yield db.collection("items").find({}).toArray();
-            else
-                data = yield db
-                    .collection("items")
-                    .find({ _id: new mongodb_1.ObjectID(type) })
-                    .toArray();
-            reply.send(data);
-            client.close();
-        }));
+        if (type === "*" || mongodb_1.ObjectID.isValid(type))
+            mongodb_2.default(({ db, client }) => tslib_1.__awaiter(this, void 0, void 0, function* () {
+                let data;
+                if (type === "*")
+                    data = yield db.collection("items").find({}).toArray();
+                else
+                    data = yield db
+                        .collection("items")
+                        .find({ _id: new mongodb_1.ObjectID(type) })
+                        .toArray();
+                reply.send(data);
+                client.close();
+            }));
+        else
+            reply.code(404).send("Sorry, but your request is invalid");
     });
 }
 exports.default = basicRoute;
--- ts/api/item.ts
+++ ts/api/item.ts
@@ -10,17 +10,20 @@ function basicRoute(server: FastifyInstance): void {
   server.get("/getItem", (req: FastifyRequest, reply: FastifyReply) => {
     const total: StringQuery = req.query as StringQuery;
     const type: string = total.type;
-    mongodbFunc(async ({ db, client }) => {
-      let data;
-      if (type === "*") data = await db.collection("items").find({}).toArray();
-      else
-        data = await db
-          .collection("items")
-          .find({ _id: new ObjectID(type) })
-          .toArray();
-      reply.send(data);
-      client.close();
-    });
+    if (type === "*" || ObjectID.isValid(type))
+      mongodbFunc(async ({ db, client }) => {
+        let data;
+        if (type === "*") data = await db.collection("items").find({}).toArray();
+        else
+          data = await db
+            .collection("items")
+            .find({ _id: new ObjectID(type) })
+            .toArray();
+        reply.send(data);
+        client.close();
+      });
+    // eslint-disable-next-line
+    else reply.code(404).send("Sorry, but your request is invalid");
   });
 }
 
